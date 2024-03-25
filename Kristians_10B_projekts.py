0f52875d7e0a9044b994c838c24cf086c503aa40
Kristians Volks <152172006+Kikamons@users.noreply.github.com>
2024-03-25 13:11:14 +0200

    Add files via upload

--- Kristians_10B_projekts.py
+++ Kristians_10B_projekts.py
@@ -2,12 +2,14 @@ print("Ir sācies vasaras brīvlaiks!")
 print("Tu ar saviem diviem draugiem, Tomasu un Jāni, esat izdomājuši apmeklēt spocīgu māju!")
 print("Vai vēlies iet iekšā viens pats, ar draugiem vai iet mājās?")
 print("")
-print("Izvēles opcijas: Viens, Kopā vai Iet mājās")
+print("Izvēles opcijas: Viens, Kopā vai Iet mājās:")
 nauda = 0
-sakums = input("> ")
-if sakums.lower().strip() == "viens":
-    print("Viens")
-elif sakums.lower().strip() == "kopā":
-    print("Kopā")
-else:
-    print("Mājas")
+def galvenais():    
+    while True:
+        sakums = input("> ")
+        if sakums.lower().strip() == "viens":
+            print("Viens")
+        elif sakums.lower().strip() == "kopā":
+            print("Kopā")
+        else:
+            print("Tu devies mājās!")
